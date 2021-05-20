a8ac1f7a4b3b1f8f67c79d363510082e4dcd29cd
GitCat-ux <arjunsankpal9@gmail.com>
2021-05-21 02:00:22 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './style.css';
 export default function App() {
   return (
     <>
+    <div className="body">
       <div className="wrap">
         <h1>Hi!</h1>
-        <ul>
+        <ul id ="sftList">
           <li>Clean your hands often</li>
           <li>Cough or sneeze in your bent elbow - not your hands!</li>
           <li>Limit social gatherings and time spent in crowded places</li>
           <li>Clean and disinfect frequently touched objects and surfaces</li>
         </ul>
         <div className="video">
-          <iframe width="350" height="300" src="https://www.youtube.com/embed/xVu_I6WCsto" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="350" height="200" src="https://www.youtube.com/embed/xVu_I6WCsto" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
+      </div>
       </div>
     </>
   );
